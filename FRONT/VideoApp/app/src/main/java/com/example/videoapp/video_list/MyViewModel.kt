32696c63cd5171a35611934da6bcb9e60e3f2cf8
MKSonny <minson9@naver.com>
2023-03-17 20:07:42 +0900

    ask_layout에 제목과 글을 작성할 수 있는 칸 만듬

--- FRONT/VideoApp/app/src/main/java/com/example/videoapp/video_list/MyViewModel.kt
+++ FRONT/VideoApp/app/src/main/java/com/example/videoapp/video_list/MyViewModel.kt
@@ -25,12 +25,6 @@ class MyViewModel : ViewModel() {
     // add, update, delete 행동 구분을 위해서 사용
     var itemNotifiedType: ItemNotify = ItemNotify.ADD
 
-    init {
-//        addItem(Uri.parse("https://firebasestorage.googleapis.com/v0/b/videotest-ffe11.appspot.com/o/uploads%2Fcctv_2023-03-09T13%3A49%3A42.mp4?alt=media&token=89fb33b2-3096-4f2b-a0fe-254bb8d01b51"))
-//        addItem(Uri.parse("https://firebasestorage.googleapis.com/v0/b/videotest-ffe11.appspot.com/o/uploads%2Fcctv_2023-03-09T13%3A49%3A42.mp4?alt=media&token=89fb33b2-3096-4f2b-a0fe-254bb8d01b51"))
-//        addItem(Uri.parse("https://firebasestorage.googleapis.com/v0/b/videotest-ffe11.appspot.com/o/uploads%2Fcctv_2023-03-09T13%3A49%3A42.mp4?alt=media&token=89fb33b2-3096-4f2b-a0fe-254bb8d01b51"))
-    }
-
     @RequiresApi(Build.VERSION_CODES.O)
     fun sortVideos(unsortedVideos : List<Uri>) {
         val sortedVideos = unsortedVideos.sortedBy { uri ->
