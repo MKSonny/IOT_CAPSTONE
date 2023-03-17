9d06fd5937ecdf1fd2dd775d1c700467a77bd9ac
MKSonny <minson9@naver.com>
2023-03-17 20:30:26 +0900

    우선 문의페이지와 파이어베이스 연동 성공 바텀네비게이션 추가 고려

--- FRONT/VideoApp/app/src/main/java/com/example/videoapp/fragments/AskFragment.kt
+++ FRONT/VideoApp/app/src/main/java/com/example/videoapp/fragments/AskFragment.kt
@@ -1,6 +1,8 @@
 package com.example.videoapp.fragments
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -8,6 +10,8 @@ import androidx.navigation.fragment.findNavController
 import com.example.videoapp.R
 import com.example.videoapp.databinding.AskLayoutBinding
 import com.example.videoapp.video_list.MyViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class AskFragment : Fragment(R.layout.ask_layout) {
     private val viewModel : MyViewModel by activityViewModels()
@@ -16,5 +20,25 @@ class AskFragment : Fragment(R.layout.ask_layout) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = AskLayoutBinding.bind(view)
+
+        val db = Firebase.firestore
+        val asksCollectionRef = db.collection("Ask")
+
+        binding.button.setOnClickListener {
+            val title = binding.askTitle.text.toString()
+            val context = binding.askText.text.toString()
+
+            val askData = hashMapOf(
+                "title" to title,
+                "context" to context
+            )
+
+            asksCollectionRef.add(askData).addOnSuccessListener { documentReference ->
+                val documentId = documentReference.id
+                Log.d(TAG, "Document added with ID: $documentId")
+            }
+
+            findNavController().navigate(R.id.action_askFragment_to_videosFragment)
+        }
     }
 }
