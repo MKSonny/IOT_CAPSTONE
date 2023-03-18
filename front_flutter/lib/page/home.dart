26d4b4a4a6eb302a1a0b5237fcf2bd9560c79852
MKSonny <minson9@naver.com>
2023-03-18 16:02:46 +0900

    바텀네비게이션바 추가

--- front_flutter/lib/page/home.dart
+++ front_flutter/lib/page/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,10 +11,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Map<String, String>> datas = [];
+  late int _currentPageIndex;
 
   @override
   void initState() {
     super.initState();
+    _currentPageIndex = 0;
       datas = [
         {
           "image": "assets/images/ara-1.jpg",
@@ -117,28 +120,45 @@ class _HomeState extends State<Home> {
 
   Widget _bodyWidget() {
     return ListView.separated(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
       itemBuilder: (BuildContext _context, int index) {
         return Container(
+          padding: const EdgeInsets.symmetric(vertical: 10), // vertical: 위, 아래에만 적용된다.
           child: Row(
             children: [ClipRRect(
               borderRadius: BorderRadius.all(Radius.circular(10)),
               child: Image.asset(datas[index]["image"]!, width: 100, height: 100,)
               ),
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                Text(datas[index]["title"]!),
-                Text(datas[index]["location"]!),
-                Text(datas[index]["price"]!),
-                Row( // 세로가 아닌 가로로 하트와 좋아요수를 놔야 되기 때문에 Row로 감싸준다.
+              Expanded(
+                child: Container(
+                  // color: Colors.blue,
+                  height: 100,
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SvgPicture.asset("assets/svg/heart_off.svg", width: 13, height: 13,),
-                    SizedBox(width: 5), // 하트와 좋아요수 사이에 빈공간을 만들어준다.
-                    Text(datas[index]["likes"]!),
-                  ],
-                ),
-              ]),)
+                    // overflow: TextOverflow.ellipsis: 문장이 너무 길면 ...으로
+                  Text(datas[index]["title"]!, style: TextStyle(fontSize: 15), overflow: TextOverflow.ellipsis,),
+                    SizedBox(height: 5,),
+                  Text(datas[index]["location"]!, style: TextStyle(fontSize: 12, color: Colors.black.withOpacity(0.3)),),
+                    SizedBox(height: 5,),
+                    Text(calcStringToWon(datas[index]["price"]!), style: TextStyle(fontWeight: FontWeight.w500),),
+                  Expanded( // 위의 3개의 텍스트를 제외한 나머지 공간을 가져간다.
+                    child: Container(
+                      // color: Colors.red,
+                      child: Row( // 세로가 아닌 가로로 하트와 좋아요수를 놔야 되기 때문에 Row로 감싸준다.
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          SvgPicture.asset("assets/svg/heart_off.svg", width: 13, height: 13,),
+                          SizedBox(width: 5), // 하트와 좋아요수 사이에 빈공간을 만들어준다.
+                          Text(datas[index]["likes"]!),
+                        ],
+                      ),
+                    ),
+                  ),
+                ]),),
+              )
               ],
             )
           );
@@ -149,12 +169,48 @@ class _HomeState extends State<Home> {
       itemCount: 10);
   }
 
+  final oCcy = new NumberFormat("#,###", "ko_KR");
+  String calcStringToWon(String priceString) {
+    return "${oCcy.format(int.parse(priceString))}원";
+  }
+
+  BottomNavigationBarItem _bottomNavigationBarItem(String iconName, String label) {
+    return BottomNavigationBarItem(
+      icon: Padding(
+        padding: const EdgeInsets.only(bottom: 5),
+        child: SvgPicture.asset("assets/svg/${iconName}_off.svg", width: 22,),
+      ),
+      label: label,
+    );
+  }
+
+  Widget _bottomNavigationBarwidget() {
+    return BottomNavigationBar(
+      // animation
+      type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+        currentIndex: _currentPageIndex,
+        selectedItemColor: Colors.black,
+        items: [
+          _bottomNavigationBarItem("home", "홈"),
+          _bottomNavigationBarItem("notes", "동네 생활"),
+          _bottomNavigationBarItem("location", "내 근처"),
+          _bottomNavigationBarItem("chat", "채팅"),
+          _bottomNavigationBarItem("user", "나의 당근"),
+        ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appbarWidget(),
       body: _bodyWidget(),
-      // bottomNavigationBar: Container(),
+      bottomNavigationBar: _bottomNavigationBarwidget(),
     );
   }
 }
