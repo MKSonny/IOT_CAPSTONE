7ed16cc6eea034a5cdd566e8d5fcce1ebe7a25e6
MKSonny <minson9@naver.com>
2023-03-23 20:46:26 +0900

    알림 안드로이드 성공, 아이폰 실패

--- front_flutter/lib/main.dart
+++ front_flutter/lib/main.dart
@@ -1,14 +1,44 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:front_flutter/page/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+ 
+
+  print("Handling a background message: ${message.messageId}");
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  }); 
+
   runApp(const MaterialApp(
     home: HomePage(),
     // debugShowCheckedModeBanner: false,
